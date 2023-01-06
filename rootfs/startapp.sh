74932ac23509c7bc1005a1909303c8e522925a56
Albert Kiskorov <me@albi.io>
2023-01-06 13:23:35 +0700

    GUI GTK trackma

---
+++ rootfs/startapp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# symlink config
+if [ ! -e /root/.trackma ]; then
+    ln -s /config /root/.trackma
+fi
+
+# configure account and settings
+python /opt/trackma/config.py
+
+exec python /usr/bin/trackma-gtk
